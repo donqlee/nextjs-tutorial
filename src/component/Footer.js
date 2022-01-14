a639efbe571584e9ed8680e19e15c3c2745f5909
donqlee <donqlee88@gmail.com>
2022-01-14 18:35:07 +0900

    nextjs page layout

---
+++ src/component/Footer.js
@@ -0,0 +1,3 @@
+export default function Footer(){
+    return <div>Copyright 돈큐리. All rights reserced</div>;
+}
---
+++ src/component/ItemList.js
@@ -0,0 +1,27 @@
+import { Grid } from "semantic-ui-react"
+import styles from './ItemList.module.css'
+
+export default function ItemList({list}) {
+    return (
+        <div>
+            <Grid columns={3}>
+                <Grid.Row>
+                    {
+                        list.map((item) => (
+                            <Grid.Column>
+                                <img src={item.image_link} alt={item.name} className={styles.img_item}/>
+                                <strong className={styles.tit_item}>{item.name}</strong>
+                                <span>
+                                    {item.category} {item.product_type}
+                                </span>
+                                <strong>${item.price}</strong>
+                            </Grid.Column>
+
+                        ))
+                    }
+
+                </Grid.Row>
+            </Grid>
+        </div>
+    )
+}
